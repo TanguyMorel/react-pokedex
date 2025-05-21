97d54535ddd10442f39f5f30fedca52a080ae4d8
Tanguy <tanguymorel.pro@gmail.com>
2025-05-21 11:39:29 +0200

    add: displaying PV by fetching datas of each poke

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const PokemonCard = ({name, i}) => {
+const PokemonCard = ({ name, i }) => {
+  const [hp, setHp] = useState(null)
+  const [type, setType] = useState(null)
+  const APIURL = `https://pokeapi.co/api/v2/pokemon/${i}`
+
+  useEffect(() => {
+    async function fetchDetails() {
+      try {
+         const res = await fetch(APIURL)
+      const data = await res.json()
+
+      const hpObj = data.stats.find(stat => stat.stat.name === "hp")
+      const hpStat = hpObj.base_stat 
+      setHp(hpStat)
+
+      const typeNames = data.types.map(type => type.type.name)
+      setType(typeNames)
+
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    fetchDetails()
+  }, [i])
+
+
   return (
     <div className="pokemon-card">
-        <h2>{name}</h2>
-       <img
+      <h2>{name}</h2>
+      <img
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png`}
         alt={`Image de ${name}`}
       />
+      <p>PV : {hp}</p>
+      <p>Types : {type}</p>
     </div>
   )
 }
