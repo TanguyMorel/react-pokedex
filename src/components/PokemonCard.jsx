40f9a4084001942f199b6bd74911547c44a2e314
Tanguy <tanguymorel.pro@gmail.com>
2025-05-21 11:53:27 +0200

    add: different types by mapping the array

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,21 +2,21 @@ import React, { useEffect, useState } from 'react'
 
 const PokemonCard = ({ name, i }) => {
   const [hp, setHp] = useState(null)
-  const [type, setType] = useState(null)
+  const [type, setType] = useState([])
   const APIURL = `https://pokeapi.co/api/v2/pokemon/${i}`
 
   useEffect(() => {
     async function fetchDetails() {
       try {
-         const res = await fetch(APIURL)
-      const data = await res.json()
+        const res = await fetch(APIURL)
+        const data = await res.json()
 
-      const hpObj = data.stats.find(stat => stat.stat.name === "hp")
-      const hpStat = hpObj.base_stat 
-      setHp(hpStat)
+        const hpObj = data.stats.find(stat => stat.stat.name === "hp")
+        const hpStat = hpObj.base_stat
+        setHp(hpStat)
+        const typeNames = data.types.map(type => type.type.name)
+        setType(typeNames)
 
-      const typeNames = data.types.map(type => type.type.name)
-      setType(typeNames)
 
       } catch (e) {
         console.error(e)
@@ -34,7 +34,12 @@ const PokemonCard = ({ name, i }) => {
         alt={`Image de ${name}`}
       />
       <p>PV : {hp}</p>
-      <p>Types : {type}</p>
+      <div className="types" style={{display:"flex", gap:"10px"}}>
+        {type.map((type, index) => (
+          <span key={index} className="type-badge" style={{padding: "4px 10px"}}>{type}</span>
+        ))}
+      </div>
+
     </div>
   )
 }
